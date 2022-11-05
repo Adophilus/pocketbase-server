9b1feae4a1b16183f7255d1842a2dc63b841fd09
Adophilus <uchenna19of@gmail.com>
2022-11-05 03:58:21 +0100

    modded the init script

--- src/init.ts
+++ src/init.ts
@@ -5,11 +5,6 @@ dotenv.config()
 
 const main = async () => {
   const pocketbaseClient = new PocketBase(process.env.POCKETBASE_URL)
-  await pocketbaseClient.admins.create({
-    email: process.env.POCKETBASE_ADMIN_EMAIL,
-    password: process.env.POCKETBASE_ADMIN_PASSWORD,
-    passwordConfirm: process.env.POCKETBASE_ADMIN_PASSWORD
-  })
 }
 
 main()
