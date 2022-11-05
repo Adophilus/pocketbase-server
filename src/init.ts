9163c712fc910c7a75660d1643558f2fab9c82ad
Adophilus <uchenna19of@gmail.com>
2022-11-05 03:51:12 +0100

    added a new script

--- src/init.ts
+++ src/init.ts
@@ -1,1 +1,15 @@
-console.log('This is the main script')
+import PocketBase from 'pocketbase'
+import * as dotenv from 'dotenv'
+
+dotenv.config()
+
+const main = async () => {
+  const pocketbaseClient = new PocketBase(process.env.POCKETBASE_URL)
+  await pocketbaseClient.admins.create({
+    email: process.env.POCKETBASE_ADMIN_EMAIL,
+    password: process.env.POCKETBASE_ADMIN_PASSWORD,
+    passwordConfirm: process.env.POCKETBASE_ADMIN_PASSWORD
+  })
+}
+
+main()
